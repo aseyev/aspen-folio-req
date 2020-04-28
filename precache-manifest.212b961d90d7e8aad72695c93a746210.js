0ef5b8214415912f29544ac067fc0e2425872a3e
aseyev <aseyev.1982@gmail.com>
2020-04-28 14:21:07 +0700

    Updates

---
+++ precache-manifest.212b961d90d7e8aad72695c93a746210.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06544b6be83017a30b1d2c124a4a4d7b",
+    "url": "/aspen-folio-req/index.html"
+  },
+  {
+    "revision": "f97d7ed63c5ec9f213ef",
+    "url": "/aspen-folio-req/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ace31d851094fb2b26db",
+    "url": "/aspen-folio-req/static/js/2.8376876c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/aspen-folio-req/static/js/2.8376876c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f97d7ed63c5ec9f213ef",
+    "url": "/aspen-folio-req/static/js/main.1e6ebf08.chunk.js"
+  },
+  {
+    "revision": "1718890c6a27b291e82e",
+    "url": "/aspen-folio-req/static/js/runtime-main.48caf892.js"
+  }
+]);
