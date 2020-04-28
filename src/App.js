da41505f7293e8d5b055f03d1dc5d67f0e9da639
aseyev <aseyev.1982@gmail.com>
2020-04-29 00:13:54 +0700

    foolproof input

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import {
+    Container,
+    Box,
+    Button,
+    Card,
+    CardHeader,
+    CardActions,
+    TextField,
+    makeStyles,
+} from "@material-ui/core";
+import ReactDOM from "react-dom";
+
+const useStyles = makeStyles({
+    root: {
+        minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    header: {
+        textAlign: "center",
+    },
+    button: {
+        margin: 20,
+    },
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">npm 
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [folioId, setfolioId] = useState("00000000");
+    const classes = useStyles();
+
+    const handleKey = (e) => {
+      let value = e.target.value.trim()
+      let regexp = /^[0-9]*$/g
+      if (regexp.test(value)) {
+        if(value.length < 9) {value = '00000000' + value}
+        if(value.length > 8) {value=value.slice(-8)} 
+      }
+      else value='00000000'
+      setfolioId(value)
+    }
+    return (
+        <Container maxWidth="sm">
+            <Box>
+                <Card className={classes.root}>
+                    <CardHeader
+                        className={classes.header}
+                        title="You can get Folio PDF here"
+                        subheader="Just input 8-digits number"
+                    />
+                    <CardActions>
+                        <TextField
+                            id="outlined-number"
+                            label="8-digit number (required)"
+                            autoFocus={true}
+                            required={true}
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                            value={folioId}
+                            variant="outlined"
+                            onChange={(e) => handleKey(e)}
+                        />
+                    </CardActions>
+                    <Button
+                        className={classes.button}
+                        variant="contained"
+                        color="primary"
+                    >
+                        Request PDF
+                    </Button>
+                </Card>
+            </Box>
+        </Container>
+    );
 }
 
 export default App;
---
+++ src/request.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+export class PostRequest extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+            
+        }
+    }
+    
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default PostRequest
